3137f962a6d980f5124a2544852eec36a9190c25
nartzaa <39677740+nartzaa@users.noreply.github.com>
2019-04-07 22:24:15 +0700

    Version01

---
+++ HC-SR501_PIR_Motion_Sensor_Module_01.ino
@@ -0,0 +1,24 @@
+int ledPin= 13;
+int inputPin= 3;
+
+
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(inputPin, INPUT);
+}
+
+void loop(){
+  int value= digitalRead(inputPin);
+
+  if (value == HIGH)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(1000) ;
+  }
+
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
